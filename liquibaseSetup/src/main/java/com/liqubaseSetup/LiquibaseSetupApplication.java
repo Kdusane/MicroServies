875efb4e99cc52630cee12e50461d4ce95414864
Kdusane <kunaldsn@gmail.com>
2023-01-12 16:02:37 +0530

    lquibase

---
+++ liquibaseSetup/src/main/java/com/liqubaseSetup/LiquibaseSetupApplication.java
@@ -0,0 +1,13 @@
+package com.liqubaseSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseSetupApplication.class, args);
+	}
+
+}
